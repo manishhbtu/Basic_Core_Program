1015ad952de85eadf32bba7b2088d0b0c724f0d0
manish <manishkmar772@gmail.com>
2021-10-23 12:37:25 +0530

    Added:Distance

---
+++ src/com/company/CorejavaProgram/Distance.java
@@ -0,0 +1,9 @@
+package com.company.CorejavaProgram;
+
+public class Distance {
+    public static void main(String[] args) {
+        int x1=1, y1=5, x2=0, y2=0 ;
+        double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println(distance);
+    }
+}
